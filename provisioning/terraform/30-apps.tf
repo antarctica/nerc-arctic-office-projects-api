fe2d19eb72bd1d1d8d3d4f9cf415afaa64fe0f54
Felix Fennell <felnne@bas.ac.uk>
2019-03-18 13:54:31 +0000

    Whitelisting arctic office website azure application

--- provisioning/terraform/30-apps.tf
+++ provisioning/terraform/30-apps.tf
@@ -25,7 +25,7 @@ resource "heroku_app" "bas-arctic-office-projects-api-stage" {
     REVERSE_PROXY_PATH                 = "/arctic-office-projects/testing"
     AZURE_OAUTH_TENANCY                = "d14c529b-5558-4a80-93b6-7655681e55d6"
     AZURE_OAUTH_APPLICATION_ID         = "2b3f5c55-1a7d-4e26-a9a7-5b56b0f612f1"
-    AZURE_OAUTH_CLIENT_APPLICATION_IDS = "76ee9805-1ec6-47c8-a57d-df002c54e498"
+    AZURE_OAUTH_CLIENT_APPLICATION_IDS = "76ee9805-1ec6-47c8-a57d-df002c54e498,8646223c-46aa-4bc3-a825-562cbea4911d"
   }
 }
 
