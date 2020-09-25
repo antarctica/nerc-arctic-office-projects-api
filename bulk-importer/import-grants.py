b6f27e8fd67621653639c70ed99f4d8db95bce94
Luke McDonald <ludona@bas.ac.uk>
2020-09-25 16:24:46 +0100

    Run commands with subprocess instead
    Forgot to change this, glad the test flagged it up

--- bulk-importer/import-grants.py
+++ bulk-importer/import-grants.py
@@ -1,14 +1,18 @@
 import json
-import os
+import subprocess
 
 
 def import_grants(file):
     with open(file) as json_file:
         data = json.load(json_file)
         for project in data['data']:
-            cmd = 'flask import grant gtr %s' % (project['grant-reference'])
-            os.system(cmd)
-
+            subprocess.run(
+                ['flask',
+                 'import',
+                 'grant',
+                 'gtr',
+                 project['grant-reference']]
+                )
 
 import_grants('json/projects-2019-06-16.json')
 # python3 import-grants.py &> import.log
