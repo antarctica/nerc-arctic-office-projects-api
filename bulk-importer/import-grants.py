d1e53eff58bb3d6d347a90114f15bc588c1286cf
Luke McDonald <ludona@bas.ac.uk>
2020-09-25 16:51:16 +0100

    Check input file contains valid JSON

--- bulk-importer/import-grants.py
+++ bulk-importer/import-grants.py
@@ -2,17 +2,21 @@ import json
 import subprocess
 
 
+def json_valid(filename):
+    try:
+        json.load(open(filename))
+        return True
+    except ValueError as error:
+        print('Invalid json file: %s' % error)
+        return False
+
 def import_grants(file):
     with open(file) as json_file:
         data = json.load(json_file)
         for project in data['data']:
-            subprocess.run(
-                ['flask',
-                 'import',
-                 'grant',
-                 'gtr',
-                 project['grant-reference']]
-                )
+            subprocess.run(['flask', 'import', 'grant', 'gtr', project['grant-reference']])
 
-import_grants('json/projects-2019-06-16.json')
-# python3 import-grants.py &> import.log
+json_filename = 'json/projects-2019-06-16.json'
+if json_valid(json_filename):
+    import_grants(json_filename)
+    # python3 import-grants.py &> import.log
