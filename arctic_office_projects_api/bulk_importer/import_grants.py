5b21b26fd81eb1ef3cb98b12b9a7df6212946465
McDonald, Luke K.M <ludona@bas.ac.uk>
2020-10-07 13:11:34 +0100

    Change to absolute path

--- arctic_office_projects_api/bulk_importer/import_grants.py
+++ arctic_office_projects_api/bulk_importer/import_grants.py
@@ -32,6 +32,6 @@ def import_grants(file):
                                 project['grant-reference']], shell=False)
 
 
-json_filename = 'json/projects-2020-08-13.json'
+json_filename = '/usr/src/app/arctic_office_projects_api/bulk_importer/json/projects-2020-08-13.json'
 if json_valid(json_filename):
     import_grants(json_filename)
