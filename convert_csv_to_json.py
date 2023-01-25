7202605b298f26b96f8d285715ab3cb3207bb9ce
Jason White <jasite@bas.ac.uk>
2023-01-25 10:59:35 +0000

    Script to convert projects spreadsheet to json

---
+++ convert_csv_to_json.py
@@ -0,0 +1,39 @@
+import csv
+import json
+
+# id to start from number 
+id_counter = 438
+
+csvFilePath = r'projects.csv'
+jsonFilePath = r'projects.json'
+
+def make_json(csvFilePath, jsonFilePath):
+
+    data = []
+
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+
+        for row in csvReader:
+            row_list = list(row.items())
+
+            lead_project = 0
+
+            if(row_list[1][1] == 'Lead'):
+                lead_project = 1            
+
+            json_block = {
+                "parent-id": None,
+                "id": int(row_list[0][1]),
+                "lead-project": lead_project,
+                "grant-reference": row_list[2][1],
+                "title": row_list[3][1]
+            }
+
+            data.append(json_block)
+
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+    	jsonf.write(json.dumps(data, indent=4))
+
+
+make_json(csvFilePath, jsonFilePath)
