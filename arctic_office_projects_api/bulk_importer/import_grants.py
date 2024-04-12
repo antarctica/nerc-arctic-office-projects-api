a58bc576c1b933ec47e663658e106b97c47f9db6
Jason White <jasite@bas.ac.uk>
2024-04-12 09:10:08 +0100

    Add commas

--- arctic_office_projects_api/bulk_importer/import_grants.py
+++ arctic_office_projects_api/bulk_importer/import_grants.py
@@ -40,10 +40,8 @@ def import_grants(file):
             if grant_reference_valid(project["grant-reference"]):  # nosec
                 subprocess.run(
                     [
-                        "poetry"
-                        "install"
-                        "poetry"
-                        "run"
+                        "poetry",
+                        "run",
                         "flask",
                         "import",
                         "grant",
