ce98d2b208e69f09bd9f08a3b2f0658574332af9
Jason White <jasite@bas.ac.uk>
2025-09-23 12:26:05 +0100

    Add env var

--- arctic_office_projects_api/utils.py
+++ arctic_office_projects_api/utils.py
@@ -1,6 +1,7 @@
 # noinspection PyPackageRequirements
 import ulid
 import logging
+import os
 
 from enum import Enum
 
@@ -19,12 +20,15 @@ from flask import has_request_context, request, current_app as app
 
 class RequestFormatter(logging.Formatter):
     def format(self, record):
+
+        APP_ENABLE_REQUEST_ID = os.getenv('APP_ENABLE_REQUEST_ID')
+
         record.url = "NA"
         record.request_id = "NA"
 
         if has_request_context():
             record.url = request.url
-            if app.config["APP_ENABLE_REQUEST_ID"]:
+            if APP_ENABLE_REQUEST_ID:
                 record.request_id = request.environ.get("HTTP_X_REQUEST_ID")
 
         return super().format(record)
